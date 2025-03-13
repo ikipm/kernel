16d4f26147a5851ea28ea39454354ca598bb9d2c
Iker Pérez <ikipm0@gmail.com>
2025-03-13 23:58:36 +0100

    Update: Improved boot.asm

--- src/boot.asm
+++ src/boot.asm
@@ -1,13 +1,18 @@
 section .multiboot
 align 4
 dd 0x1BADB002     ; Multiboot identifier
-dd 0x00           ; Flags
+dd 0x00           ; Flags (0 - default config)
 dd - (0x1BADB002 + 0x00) ; Checksum
 
 section .text
 global _start
-extern main
+extern main            ; Defined in file kernel.c
 _start:
     cli                ; Disable interruptions
+    mov esp, stack_space + 8192 ; set stack pointer
     call main          ; Starts C code
     hlt                ; Stop system
+
+section .bss
+align 16
+stack_space resb 8192  ; 8KB for stack

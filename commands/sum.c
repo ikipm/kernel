ec9da9e4451eeb705d74e23eb4c1e0178198be54
Iker Pérez <ikipm0@gmail.com>
2025-04-05 01:02:30 +0200

    Add: Exception to sum.

--- commands/sum.c
+++ commands/sum.c
@@ -3,6 +3,7 @@
 #include <commands.h>
 
 int sum_command(char *args[]){
+  if(!is_number(args[1]) || !is_number(args[2])) return 1;
   int n1 = str_to_int(args[1]), n2 = str_to_int(args[2]);
   print_char('\n');
   print_int(n1+n2);
